4796a159c78186fd6206c9f2b3c6fa9faa036e57
ENISS KASPER <123799980+Enis000-hub@users.noreply.github.com>
2024-07-28 22:05:02 +0300

    Add files via upload

---
+++ .vs/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HardwareStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HardwareStore.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
